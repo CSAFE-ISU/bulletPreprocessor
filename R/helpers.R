133b751b7a7453e9a3a7b6ae2eb9497efebec26c
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-08-15 10:24:00 -0500

    sort helper functions alphabetically

--- R/helpers.R
+++ R/helpers.R
@@ -1,19 +1,46 @@
-get_land_name <- function(filename) {
-  return(stringr::str_extract(filename, "Land \\d+"))
-}
-
-get_bullet_name <- function(filename) {
-  return(stringr::str_extract(filename, "Bullet \\d+"))
-}
-
 get_barrel_name <- function(filename, study) {
   if (study == "houston") {
     return(get_houston_barrel_name(filename))
   }
 }
 
+get_bullet_name <- function(filename) {
+  return(stringr::str_extract(filename, "Bullet \\d+"))
+}
+
 get_houston_barrel_name <- function(filename) {
   return(stringr::str_extract(filename, "\\b[A-Z]+(?=\\s+Bullet)"))
+}
+
+get_land_name <- function(filename) {
+  return(stringr::str_extract(filename, "Land \\d+"))
+}
+
+make_land_card <- function(land_id, 
+                           barrel_name = NULL,
+                           bullet_name = NULL,
+                           land_name = NULL) {
+  card(
+    card_header(class = "bg-dark", paste(barrel_name, bullet_name, land_name)),
+    full_screen = TRUE,
+    rglwidgetOutput(land_id, width = "auto"),
+  )
+}
+
+make_plot_card <- function(plot_id, header_title) {
+  card(
+    card_header(class = "bg-dark", header_title),
+    full_screen = TRUE,
+    plotOutput(plot_id),
+  )
+}
+
+make_table_card <- function(table_id, header_title) {
+  card(
+    card_header(class = "bg-dark", header_title),
+    full_screen = TRUE,
+    tableOutput(table_id),
+  )
 }
 
 plot_grooves <- function(ccdata, left_groove, right_groove) {
@@ -37,31 +64,4 @@ plot_signal <- function(signal_df) {
     ylim(c(-5,5)) +
     theme_bw() +
     labs(y = "value")
-}
-
-make_land_card <- function(land_id, 
-                           barrel_name = NULL,
-                           bullet_name = NULL,
-                           land_name = NULL) {
-  card(
-    card_header(class = "bg-dark", paste(barrel_name, bullet_name, land_name)),
-    full_screen = TRUE,
-    rglwidgetOutput(land_id, width = "auto"),
-  )
-}
-
-make_table_card <- function(table_id, header_title) {
-  card(
-    card_header(class = "bg-dark", header_title),
-    full_screen = TRUE,
-    tableOutput(table_id),
-  )
-}
-
-make_plot_card <- function(plot_id, header_title) {
-  card(
-    card_header(class = "bg-dark", header_title),
-    full_screen = TRUE,
-    plotOutput(plot_id),
-  )
 }
